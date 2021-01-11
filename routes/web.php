f9ac51f24711c1f0e921b0e9103c221bd946858b
Chen Dong Meng <3a732001@gm.student.ncut.edu.tw>
2021-01-11 13:49:56 +0800

    查詢資料 use all function

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    $post=new Post();
-//    $post->title ='test title';
-//    $post->content='test content';
-//    $post->save();
-    Post::create([
-        'title'=>'test title',
-        'content'=>'test content',
-    ]);
+
+    $post=Post::all();
+    dd($post);
 
 });
 
